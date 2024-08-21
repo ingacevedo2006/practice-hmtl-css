2d7e2c79fcf9e39bbdf4e43f14ba4ba991535c9d
ingacevedo2006 <sa.projects@outlook.com>
2024-08-20 22:56:21 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+function changeStyle(sheet) {
+    document.getElementById('theme-stylesheet').setAttribute('href', sheet);
+  // La función changeStyle() cambia el atributo href de la hoja de estilo que se está usando (#theme-stylesheet) para aplicar el nuevo archivo CSS seleccionado.
+}
